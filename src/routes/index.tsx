4e765ed3ac863553e363c13cee001ebf2166175e
javayhu <javayhu@gmail.com>
2026-02-25 10:36:24 +0800

    feat: create HomePage component to consolidate homepage sections and update routing

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,4 @@
-import CallToActionSection from '@/components/blocks/calltoaction';
-import FaqSection from '@/components/blocks/faqs';
-import FeaturesSection from '@/components/blocks/features';
-import Features2Section from '@/components/blocks/features2';
-import Features3Section from '@/components/blocks/features3';
-import HeroSection from '@/components/blocks/hero';
-import IntegrationSection from '@/components/blocks/integration';
-import Integration2Section from '@/components/blocks/integration2';
-import LogoCloudSection from '@/components/blocks/logo-cloud';
-import NewsletterCard from '@/components/blocks/newsletter-card';
-import PricingSection from '@/components/blocks/pricing';
-import StatsSection from '@/components/blocks/stats';
-import TestimonialsSection from '@/components/blocks/testimonials';
+import { HomePage } from '@/components/blocks/homepage';
 import { websiteConfig } from '@/config/website';
 import { seo } from '@/lib/seo';
 import { getCanonicalUrl } from '@/lib/urls';
@@ -42,23 +30,3 @@ export const Route = createFileRoute('/')({
   },
   component: HomePage,
 });
-
-function HomePage() {
-  return (
-    <div className="flex flex-col">
-      <HeroSection />
-      <LogoCloudSection />
-      <StatsSection />
-      <Features3Section />
-      <FeaturesSection />
-      <CallToActionSection />
-      <Features2Section />
-      <IntegrationSection />
-      <Integration2Section />
-      <PricingSection />
-      <FaqSection />
-      <TestimonialsSection />
-      <NewsletterCard />
-    </div>
-  );
-}
